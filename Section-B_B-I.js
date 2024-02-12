e79801a09ebdaa3b7a1960f86e7a54498c527f72
harismallick <mallick.haris@gmail.com>
2024-02-12 00:10:32 +0000

    switch-case for question B-I.

--- Section-B_B-I.js
+++ Section-B_B-I.js
@@ -1,58 +1,60 @@
-// var string = prompt("Enter your statement here:");
+// Switch Case implementation of answer:
+var string = prompt("Enter your statement here:");
 
-// var vowelCount = 0;
-// var spaceCount = 0;
+var vowelCount = 0;
+var spaceCount = 0;
 
-// for (var i = 0; i < string.length; i++) {
-//     switch(string[i]) {
-//         case string[i] == "a":
-//             vowelCount += 1;
-//             break
-//         case string[i] == "e":
-//             vowelCount += 1;
-//             break    
-//         case string[i] == "i":
-//             vowelCount += 1;
-//             break
-//         case string[i] == "o":
-//             vowelCount += 1;
-//             break   
-//         case string[i] == "u":
-//             vowelCount += 1;
-//             break   
-//         case string[i] == " ":
-//             spaceCount += 1;
-//             break   
-//     }
-// }
-// if (spaceCount >= 4) {
-//     document.write(spaceCount, vowelCount);
-// } else {
-//     document.write("Not enough words in the string.");
-// }
-
-
-var s = prompt("Enter a sentence");
-var l = s.length;
-var vowelCount = 0;  
-var spaceCount = 0;  
-var otherCharacters = 0; 
-
-for (var i = 0; i < l; i++) {
-    var char = s.charAt(i);
-    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-        vowelCount++;
-    } else if (char === " ") {
-        spaceCount++;
-    } else {
-        otherCharacters++;
+for (var i = 0; i < string.length; i++) {
+    switch(string[i]) {
+        case "a":
+            vowelCount += 1;
+            break
+        case "e":
+            vowelCount += 1;
+            break    
+        case "i":
+            vowelCount += 1;
+            break
+        case "o":
+            vowelCount += 1;
+            break   
+        case "u":
+            vowelCount += 1;
+            break   
+        case " ":
+            spaceCount += 1;
+            break   
     }
 }
 if (spaceCount >= 4) {
-    document.write("Number of Vowels = " + vowelCount + "<br>");
-    document.write("Number of Spaces = " + spaceCount + "<br>");
+    document.write("Number of spaces in the sentence: " + spaceCount + "<br>");
+    document.write("Number of vowels in the sentence: " + vowelCount + "<br>");
 } else {
-    document.write("The given sentence needs to have 5 words or more.");
+    document.write("Not enough words in the string.");
 }
+
+// If-Else implementation of answer:
+// var s = prompt("Enter a sentence");
+// var l = s.length;
+// var vowelCount = 0;  
+// var spaceCount = 0;  
+// var otherCharacters = 0; 
+
+// for (var i = 0; i < l; i++) {
+//     var char = s.charAt(i);
+//     if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//         vowelCount++;
+//     } else if (char === " ") {
+//         spaceCount++;
+//     } else {
+//         otherCharacters++;
+//     }
+// }
+// if (spaceCount >= 4) {
+//     document.write("Number of Vowels = " + vowelCount + "<br>");
+//     document.write("Number of Spaces = " + spaceCount + "<br>");
+// } else {
+//     document.write("The given sentence needs to have 5 words or more.");
+// }
 // My name is Rayyan Mallick
 
